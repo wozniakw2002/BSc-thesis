ec39d4720d5694e9fb4b9d578ab36d2384898bc2
Wiktor <103779975+wozniakw2002@users.noreply.github.com>
2024-11-14 21:00:00 +0100

    requirements

--- Modules/StatisticsModule.py
+++ Modules/StatisticsModule.py
@@ -1,5 +1,6 @@
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, auc
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, auc, roc_curve
 import numpy as np
+import matplotlib.pyplot as plt
 class Statistics:
     
     def accuracy(Y: np.array, Y_pred: np.array) -> float:
@@ -21,3 +22,19 @@ class Statistics:
     # def auc(Y: np.array, Y_pred: np.array) -> float:
     #     auc_value = auc(Y, Y_pred) # TP/(TP + FN)
     #     return auc_value
+
+    def roc_curve(Y, Y_pred):
+        fpr, tpr, _ = roc_curve(Y, Y_pred) 
+        roc_auc = auc(fpr, tpr)
+        # Plot the ROC curve
+        plt.figure()  
+        plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], 'k--', label='No Skill')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve for Breast Cancer Classification')
+        plt.legend()
+        plt.show()
+        
