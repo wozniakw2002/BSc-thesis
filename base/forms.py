c12a6c8026fb4d5c42e47018c5955055569e27ce
SebastianTrojan <115067561+SebastianTrojan@users.noreply.github.com>
2024-12-16 22:39:24 +0100

    handlig invalid forms

--- base/forms.py
+++ base/forms.py
@@ -5,5 +5,5 @@ class ImageUploadForm(forms.Form):
     visualizations = forms.MultipleChoiceField(
         choices=[('gradcam', 'GradCam'), ('lime', 'LIME'), ('shap', 'SHAP')],
         widget=forms.CheckboxSelectMultiple,
-        required=True
+        required=False
     )
