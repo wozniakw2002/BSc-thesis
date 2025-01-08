03917f6135dc3a7ae38ab2c6ccd8ff501e32f378
SebastianTrojan <115067561+SebastianTrojan@users.noreply.github.com>
2025-01-08 18:01:48 +0100

    django improved and new model

--- base/singleton.py
+++ base/singleton.py
@@ -10,7 +10,7 @@ class ModelSingleton:
     @staticmethod
     def get_model():
         if ModelSingleton._model is None:
-            model_path = os.path.join(settings.BASE_DIR, 'model.h5')
+            model_path = os.path.join(settings.BASE_DIR, 'model_weights.weights.h5')
             ModelSingleton._model = Model.example_model()
             ModelSingleton._model.load_weights(model_path)
         return ModelSingleton._model
